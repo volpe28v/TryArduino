6a01acc2a668f59f208e30033b7b2518864fe6fe
naoki kodama <naoki.koda@gmail.com>
2015-03-01 00:38:04 +0900

    LEDのアナログ制御を２つにした

--- c_lang/sketch_led_analog/sketch_led_analog.ino
+++ c_lang/sketch_led_analog/sketch_led_analog.ino
@@ -1,18 +1,21 @@
 const int LED = 9; // PWM が対応したとこじゃないとダメ
+const int LED2 = 10;
 int i = 0;
 
 void setup() {
   pinMode(LED, OUTPUT);
+  pinMode(LED2, OUTPUT);
 }
 
 void loop() {
   for (i = 0; i < 255; i++){
     analogWrite(LED,i);
-    delay(10);
-    
+    analogWrite(LED2,i);
+    delay(10);    
   }
   for (i = 255; i > 0; i--){
     analogWrite(LED,i);
+    analogWrite(LED2,i);
     delay(10);
   }
 }
